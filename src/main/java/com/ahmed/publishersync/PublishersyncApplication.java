66b24058b4f6ad0d737427ca397fd1d6b339b941
rem <ahmedelsherif.osama@gmail.com>
2025-11-19 10:25:34 +0200

    Initial commit: Spring Boot Publisher Sync skeleton

---
+++ src/main/java/com/ahmed/publishersync/PublishersyncApplication.java
@@ -0,0 +1,13 @@
+package com.ahmed.publishersync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishersyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishersyncApplication.class, args);
+	}
+
+}
